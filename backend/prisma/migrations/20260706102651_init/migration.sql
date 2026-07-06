73ed771b2561297c95892cf19e1f4c561b3cb3ab
Jackline <jackline.kerketta@practo.com>
2026-07-06 15:57:25 +0530

    Add Prisma schema and SQLite setup

---
+++ backend/prisma/migrations/20260706102651_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "read" BOOLEAN NOT NULL DEFAULT false
+);
